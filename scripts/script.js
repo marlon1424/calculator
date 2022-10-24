213dc282ef16090624381354295b72bc1779a9fc
Marlon Blackman <marlonblackman24@gmail.com>
2022-10-24 02:16:30 +0100

    adds functionality to calculator

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,75 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operaterButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById('equalsBtn');
+const clearButton = document.getElementById('clearBtn');
+const lastOperationScreen = document.getElementById("lastOperationScreen");
+const currentOperationScreen = document.getElementById("currentOperationScreen");
+
+equalsButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+
+numberButtons.forEach((button) => 
+    button.addEventListener('click', () => appendNumber(button.textContent))
+);
+
+operaterButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+function appendNumber(number) {
+    if(currentOperationScreen.textContent === '0' || shouldResetScreen){
+        resetScreen();
+    }
+    currentOperationScreen.textContent += number;
+}
+
+function resetScreen() {
+    currentOperationScreen.text = '';
+    shouldResetScreen = false;
+}
+
+function clear() {
+    currentOperationScreen.textContent = '';
+    lastOperationScreen.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
+}
+
+function setOperation(operator){
+    firstOperand =  currentOperationScreen.textContent;
+    currentOperation = operator;
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+    currentOperationScreen = '';
+    shouldResetScreen = true;
+    if (currentOperation !== null) {
+        evaluate();
+    }
+}
+
+function evaluate(){
+    if (currentOperation === null || shouldResetScreen){
+        return 
+    } 
+    
+    secondOperand = currentOperationScreen.textContent;
+        currentOperationScreen.textContent = roundResult(
+            operate(currentOperation, firstOperand, secondOperand));
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+    currentOperation = null;
+
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
+}
+
+
 function add(a,b){
     return a + b;
 }
@@ -14,32 +86,20 @@ function divide(a,b){
     return a / b;
 }
 
-function operate(){
-    let operand1 = parseInt(prompt("Enter first number"));
-    let operand2 = parseInt(prompt("Enter second number"));
-    let calculate = prompt("Type in an operation");
-
-    switch(calculate){
-        case "add" :
-            console.log(add(operand1, operand2));
-            break;
-        case "subtract" :
-            console.log(subtract(operand1, operand2));
-            break;
-        
-        case "multiply" : 
-            console.log(multiply(operand1, operand2));
-            break;
-        
-        case "divide" :
-            console.log(divide(operand1, operand2));
-            break;
-
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '×':
+            return multiply(a, b);
+        case '÷':
+            if (b === 0) return null
+            else return divide(a, b);
         default:
-            console.log("That isn't an operation");
-    
+            return null;
     }
-
 }
-
-operate();
